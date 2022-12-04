02714d870975967024aa2ccc1435f6b096f347a1
reybahl <bahl.reyansh@gmail.com>
2022-12-04 14:07:44 -0500

    Begin combining

--- process_image.js
+++ process_image.js
@@ -1,1 +1,11 @@
-/* Combine selecting area, pan-sharpening, illumination, topographic correction, and spectral indices */
+var select_area = require("users/bahlreyansh/SOC:select");
+
+
+function process_image(long1, lat1, long2, lat2) {
+	/* Combine selecting area, pan-sharpening, illumination, topographic correction, and spectral indices */
+
+	var img = select_area.select(long1, lat1, long2, lat2);
+	
+}
+
+process_image(-63.937781455384815, -2.8582106566354333, -63.319800498353565, -2.575630995105349);
--- select_area.js
+++ select_area.js
@@ -1,5 +1,5 @@
-function select(long1, long2, lat1, lat2) {
-	var rectangle = ee.Geometry.Rectangle([long1, long2, lat1, lat2]);
+function select(long1, lat1, long2, lat2) {
+	var rectangle = ee.Geometry.Rectangle([long1, lat1, long2, lat2]);
 
 	var dataset = 'LANDSAT/LC08/C01/T1_TOA';
 
@@ -14,5 +14,7 @@ function select(long1, long2, lat1, lat2) {
 	return img.clip(rectangle);
 }
 // testing
-var img = select(-63.937781455384815, -2.8582106566354333, -63.319800498353565, -2.575630995105349);
-Map.addLayer(img, {}, "sample selection" )
+// var img = select(-63.937781455384815, -2.8582106566354333, -63.319800498353565, -2.575630995105349);
+// Map.addLayer(img, {}, "sample selection" )
+
+exports.select = select;
