3f176d85aa14c97bfe1debbb0e514603017be94e
reybahl <bahl.reyansh@gmail.com>
2022-12-04 18:24:17 -0500

    Export to drive

--- process_image.js
+++ process_image.js
@@ -4,9 +4,8 @@ var topographic_correction = require("users/bahlreyansh/SOC:SCS_C_correction");
 var spectral_indices = require("users/bahlreyansh/SOC:spectral_indices");
 
 
-function process_image_TOA(long1, lat1, long2, lat2) {
-
-	/* Combine selecting area, pan-sharpening, and spectral indices for TOA images */
+function process_image(long1, lat1, long2, lat2) {
+	/* Combine selecting area, pan-sharpening, illumination, topographic correction, and spectral indices */
 
 	var img = select_area.select(long1, lat1, long2, lat2);
 
@@ -16,7 +15,11 @@ function process_image_TOA(long1, lat1, long2, lat2) {
 
 	var indices = spectral_indices.compute_indices(img);
 
-	print(indices);
+	return indices;
 }
 
-print(process_image(-63.937781455384815, -2.8582106566354333, -63.319800498353565, -2.575630995105349));
+var processed = process_image(-61.937781455384815, -2.8582106566354333, -61.319800498353565, -2.575630995105349);
+
+Export.table.toDrive(
+  ee.FeatureCollection([ee.Feature(null,processed)])
+);
