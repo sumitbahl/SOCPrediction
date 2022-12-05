557502d8949060b30fe8d39499d1ca9eacd5ee13
reybahl <bahl.reyansh@gmail.com>
2022-12-05 15:31:49 -0500

    Export batches of data to drive

--- process_image.js
+++ process_image.js
@@ -2,9 +2,9 @@ var select_area = require("users/bahlreyansh/SOC:select");
 var pan_sharpening = require("users/bahlreyansh/SOC:pan_sharpening");
 var topographic_correction = require("users/bahlreyansh/SOC:SCS_C_correction");
 var spectral_indices = require("users/bahlreyansh/SOC:spectral_indices");
+var data = require("users/bahlreyansh/SOC:data").data;
 
-
-function process_image(long1, lat1, long2, lat2) {
+function process_image(long1, lat1, long2, lat2, MU_GLOBAL) {
 	/* Combine selecting area, pan-sharpening, illumination, topographic correction, and spectral indices */
 
 	var img = select_area.select(long1, lat1, long2, lat2);
@@ -15,11 +15,21 @@ function process_image(long1, lat1, long2, lat2) {
 
 	var indices = spectral_indices.compute_indices(img);
 
-	return indices;
+	return indices.set('MU_GLOBAL', MU_GLOBAL);
 }
 
-var processed = process_image(-61.937781455384815, -2.8582106566354333, -61.319800498353565, -2.575630995105349);
+var features = [];
+
+var task = 2;
+
+for (var i = 0; i < 50; i++) {
+	var processed = process_image(data[i][1], data[i][3], data[i][2], data[i][4], data[i][0]);
+	features.push(ee.Feature(null,processed));
+}
 
 Export.table.toDrive(
-  ee.FeatureCollection([ee.Feature(null,processed)])
+	ee.FeatureCollection(features),
+	"Task" + task,
+	"/SOC/Data",
+	"" + task
 );
